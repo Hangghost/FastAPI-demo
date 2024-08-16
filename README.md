87f4d9e7935c28507eb6e8e96b7ab50eee43b6c7
Joey Chen <hangghost@gmail.com>
2024-08-17 04:39:07 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+---
+title: Demo MLOps
+emoji: 😻
+colorFrom: blue
+colorTo: red
+sdk: gradio
+sdk_version: 4.41.0
+app_file: app.py
+pinned: false
+license: cc
+---
---
+++ app.py
@@ -0,0 +1,14 @@
+from transformers import pipeline
+import gradio as gr
+
+model = pipeline("summarization")
+
+def predict(prompt):
+    summery = model(prompt)[0]['summary_text']
+    return summery
+
+with gr.Blocks() as demo:
+    textbox = gr.Textbox(placeholder="Enter text block to summarize", lines=4)
+    gr.Interface(fn=predict, inputs=textbox, outputs="text")
+
+demo.launch()
